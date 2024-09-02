1e7315935f473483cb7c0b6d1d5ee77655ca77ce
inilianbrokuuuu <hostingderz@gmail.com>
2024-09-02 09:38:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -69,7 +69,7 @@ install_jq() {
 check_token() {
   echo -e "                                                       "
   echo -e "${BLUE}[+] =============================================== [+]${NC}"
-  echo -e "${BLUE}[+]               LICENSY VALLZ OFFC             [+]${NC}"
+  echo -e "${BLUE}[+]               LICENSY Lian Hosting             [+]${NC}"
   echo -e "${BLUE}[+] =============================================== [+]${NC}"
   echo -e "                                                       "
   echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
